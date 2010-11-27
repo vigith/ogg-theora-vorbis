ee53447af2b28b4ac825a4c953e780455b20fa71
Vigith Maurice <vigith@gmail.com>
2010-11-27 13:40:52 +0530

    added missing freeing function *_clear()

--- vorbis/cu_vorbis.c
+++ vorbis/cu_vorbis.c
@@ -487,6 +487,44 @@ static PyObject * cuvorbis_list_to_wave(PyObject *self, PyObject *args) {
 };
 
 
+static PyObject * cuogg_vorbis_info_clear(PyObject *self, PyObject *args) {
+  int size;
+  vorbis_info *vi;
+  PyArg_ParseTuple(args, "s#", &vi, &size);
+  vorbis_info_clear(vi);
+  Py_INCREF(Py_None);
+  return Py_None;
+};
+
+
+static PyObject * cuogg_vorbis_block_clear(PyObject *self, PyObject *args) {
+  int size;
+  int c_out;
+  vorbis_block *vb;
+  PyArg_ParseTuple(args, "s#", &vb, &size);
+  c_out = vorbis_block_clear(vb);
+  return Py_BuildValue("i", c_out);
+};
+
+static PyObject * cuogg_vorbis_dsp_clear(PyObject *self, PyObject *args) {
+  int size;
+  vorbis_dsp_state *v;
+  PyArg_ParseTuple(args, "s#", &v, &size);
+  vorbis_dsp_clear(v);
+  Py_INCREF(Py_None);
+  return Py_None;
+};
+
+
+static PyObject * cuogg_vorbis_comment_clear(PyObject *self, PyObject *args) {
+  int size;
+  vorbis_comment *vc;
+  PyArg_ParseTuple(args, "s#", &vc, &size);
+  vorbis_comment_clear(vc);
+  Py_INCREF(Py_None);
+  return Py_None;
+};
+
 
 static PyMethodDef CuVorbisMethods[] = {
   {"make_vorbis_info", cuvorbis_make_vorbis_info, METH_VARARGS,
@@ -545,6 +583,14 @@ static PyMethodDef CuVorbisMethods[] = {
    "Encode int List of List"},
   {"list_to_wave", cuvorbis_list_to_wave, METH_VARARGS,
    "Encode List of List to wave data"},
+  {"vorbis_info_clear", cuogg_vorbis_info_clear, METH_VARARGS,
+   "Frees the internal storage for a vorbis_info structure"},
+  {"vorbis_block_clear", cuogg_vorbis_block_clear, METH_VARARGS,
+   "Frees the internal storage for a vorbis_block structure"},
+  {"vorbis_dsp_clear", cuogg_vorbis_dsp_clear, METH_VARARGS,
+   "Frees the internal storage for a vorbis_dsp_state structure"},
+  {"vorbis_comment_clear", cuogg_vorbis_comment_clear, METH_VARARGS,
+  "Frees the internal storage associated with a vorbis_comment structure"},
   {NULL, NULL, 0, NULL}
 };
 
